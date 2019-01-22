99efed975fe1090c15f0e55ef6f9470d9f9bd0f3
jortizkore <jortiz@korewireless.com>
2019-01-22 02:44:03 -0400

    task1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatFormFieldModule
+} from "@angular/material";
+@NgModule({
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatInputModule, MatFormFieldModule]
+})
+export class MaterialModule {}
